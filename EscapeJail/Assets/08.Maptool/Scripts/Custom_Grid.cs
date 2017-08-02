85f2ca729548f1f897b990194d04abba4e6a6287
용진 <wer3799@naver.com>
2017-08-03 00:55:00 +0900

    맵툴(설치,제거)

---
+++ EscapeJail/Assets/08.Maptool/Scripts/Custom_Grid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom_Grid : MonoBehaviour
+{
+    public float width = 32.0f;
+    public float height = 32.0f;
+
+    public Color gridColor = Color.white;
+
+    public Transform tilePrefab;
+
+    public TileSet tileSet;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 pos = Camera.current.transform.position;
+        Gizmos.color = gridColor;
+
+        for (float y = pos.y - 800.0f; y < pos.y + 800.0f; y += this.height)
+        {
+            Gizmos.DrawLine(new Vector3(-1000000.0f, Mathf.Floor(y / this.height) * height, 0f),
+                new Vector3(1000000.0f, Mathf.Floor(y / this.height) * height, 0f));
+
+        }
+
+        for(float x = pos.x -1200.0f;x<pos.x + 1200.0f; x += this.width)
+        {
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / this.width) * this.width, -1000000.0f, 0.0f),
+                new Vector3(Mathf.Floor(x / this.width) * this.width, 1000000.0f, 0.0f));
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EscapeJail/Assets/08.Maptool/Scripts/GridWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GridWindow : EditorWindow
+{
+    Custom_Grid grid;
+
+    public void Init()
+    {
+        grid = (Custom_Grid)FindObjectOfType(typeof(Custom_Grid));
+    }
+
+    private void OnGUI()
+    {
+        grid.gridColor = EditorGUILayout.ColorField(grid.gridColor, GUILayout.Width(200));
+    }
+
+}
