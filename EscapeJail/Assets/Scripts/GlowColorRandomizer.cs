782f0734fd7c898478e46100368245aa37b0e40c
용진 <wer3799@naver.com>
2017-08-02 18:57:08 +0900

    애니메이션확인용

---
+++ EscapeJail/Assets/Scripts/GlowColorRandomizer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteGlow))]
+public class GlowColorRandomizer : MonoBehaviour
+{
+    private SpriteGlow spriteGlow;
+
+    private void Awake ()
+    {
+        spriteGlow = GetComponent<SpriteGlow>();
+    }
+
+    private void OnEnable ()
+    {
+        spriteGlow.enabled = true;
+        spriteGlow.GlowColor = Random.ColorHSV();
+    }
+
+    private void OnDisable ()
+    {
+        spriteGlow.enabled = false;
+    }
+}
