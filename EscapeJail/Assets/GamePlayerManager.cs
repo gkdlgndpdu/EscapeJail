d244665bf4fc6c9ecbfacf265a6ff80d54883763
용진 <wer3799@naver.com>
2017-08-08 00:06:37 +0900

    방향 애니메이션 적용

---
+++ EscapeJail/Assets/GamePlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayerManager : MonoBehaviour
+{
+    public static GamePlayerManager Instance;
+
+    public GameObject player;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        player = FindPlayer();
+    }
+
+    private GameObject FindPlayer()
+    {
+        return GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+
+}
