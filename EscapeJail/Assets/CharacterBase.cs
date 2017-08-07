9c094f29757f25d719c0aa99e061e49db2784d66
용진 <wer3799@naver.com>
2017-08-07 17:05:43 +0900

    방향별 애니메이션

--- EscapeJail/Assets/CharacterBase.cs
+++ EscapeJail/Assets/CharacterBase.cs
@@ -7,10 +7,14 @@ public class CharacterBase : MonoBehaviour
 {
     //컴포넌트    
     private Rigidbody2D rb;
+    [SerializeField]
+    Animator animController;
 
     //값변수
     [SerializeField]
     private float moveSpeed=10f;
+
+
 
     private void Awake()
     {
@@ -19,6 +23,7 @@ public class CharacterBase : MonoBehaviour
     void Initialize()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
 	// Use this for initialization
@@ -40,9 +45,33 @@ public class CharacterBase : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         Vector3 moveDir = Vector3.right * h + Vector3.up * v;
-        moveDir.Normalize();
-
+       // moveDir.Normalize();
+         
+        //이동
         if(rb!=null)
         rb.velocity = moveDir * moveSpeed * Time.deltaTime;
+
+        //애니메이션
+        AnimControl(moveDir);
+    }
+
+    protected void AnimControl(Vector3 MoveDir)
+    {
+        if (animController == null) return;
+
+        float SpeedValue = Mathf.Abs(MoveDir.x) + Mathf.Abs(MoveDir.y);
+        animController.SetFloat("Speed", SpeedValue);
+        if (SpeedValue > 0.2f)
+        {
+            animController.speed = 1f;
+            animController.SetFloat("DirectionX", MoveDir.x);
+            animController.SetFloat("DirectionY", MoveDir.y);
+        }
+        else
+        {
+            animController.Play("Walk", -1,0f);
+            animController.speed = 0f;
+        }
+  
     }
 }
