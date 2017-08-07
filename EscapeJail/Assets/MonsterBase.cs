aa7b299e36a218d0474f5e35c603f543f8a8f5fb
용진 <wer3799@naver.com>
2017-08-08 01:23:50 +0900

    따라오는쥐

--- EscapeJail/Assets/MonsterBase.cs
+++ EscapeJail/Assets/MonsterBase.cs
@@ -2,18 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class MonsterBase : MonoBehaviour
-{
 
-	// Use this for initialization
-	void Start ()
+{   
+    //대상 타겟
+    private Transform target;
+
+    //컴포넌트   
+    protected Vector3 moveDir;
+    private Rigidbody2D rb;
+    [SerializeField]
+    protected Animator animator;
+
+    //속성값 (속도,hp,mp etc...)
+    [SerializeField]
+    protected float activeDistance = 10;
+    protected float moveSpeed = 1f;
+
+    // Use this for initialization
+    protected void Start ()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        if (target == null)
+            target = GamePlayerManager.Instance.player.transform;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        AddToList();
+    }
+
+    protected void Awake()
     {
-		
-	}
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    protected void AddToList()
+    {
+        MonsterManager.Instance.AddToList(this);
+    }
+
+    protected void DeleteInList()
+    {
+        MonsterManager.Instance.DeleteInList(this);
+    }
+
+    protected void OnDestroy()
+    {
+        DeleteInList();  
+    }
+
+    // Update is called once per frame
+    protected void Update ()
+    {
+        MoveRoutine();
+    }
+
+    void MoveRoutine()
+    {
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        if (target == null) return;
+        moveDir = target.position - this.transform.position;
+
+        if (rb != null)
+        {
+            rb.velocity = moveDir* moveSpeed;
+        }
+                
+
+    }
 }
---
+++ EscapeJail/Assets/MonsterManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    public static MonsterManager Instance;
+    private List<MonsterBase> monsterList;
+    
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        monsterList = new List<MonsterBase>();
+    }
+
+    public void AddToList(MonsterBase monster)
+    {
+        if (monsterList == null) return;
+        monsterList.Add(monster);
+    }
+    public void DeleteInList(MonsterBase monster)
+    {
+        if (monsterList == null) return;
+        monsterList.Remove(monster);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
