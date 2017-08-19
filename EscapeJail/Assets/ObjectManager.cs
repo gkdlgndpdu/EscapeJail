ba8c176324cd3c9690a589eaba76927fa479ea20
용진 <wer3799@naver.com>
2017-08-19 17:14:27 +0900

    머지

--- EscapeJail/Assets/ObjectManager.cs
+++ EscapeJail/Assets/ObjectManager.cs
@@ -30,58 +30,10 @@ public class ObjectManager : MonoBehaviour
 
 
         GameObject bullet =(GameObject)Resources.Load("Prefabs/Objects/Bullet");
-        bulletPool = new ObjectPool<Bullet>(null, bullet, 100);
-
+        bulletPool = new ObjectPool<Bullet>(null, bullet, 10);
 
     }
 
-    //private void MakeBullets()
-    //{
-    //    if (bulletPool == null) return;
-
-    //    for (int i = 0; i < 100; i++)
-    //    {
-    //        GameObject bullet = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Objects/Bullet"), bulletParent);
-
-    //        if (bullet != null)
-    //        {
-    //            Bullet playerBullet = bullet.GetComponent<Bullet>();
-    //            playerBullet.gameObject.SetActive(false);
-    //            bulletPool.Add(playerBullet);
-    //        }
-    //    }
-
-    //}
-
-    //private void MakeEffects()
-    //{
-    //    if (effectPool == null) return;
-
-    //    for (int i = 0; i < 100; i++)
-    //    {
-    //        GameObject effect = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Objects/ExplosionEffect"), bulletParent);
-
-    //        if (effect != null)
-    //        {
-    //            ExplosionEffect Expeffect = effect.GetComponent<ExplosionEffect>();
-    //            Expeffect.gameObject.SetActive(false);
-    //            effectPool.Add(playerBullet);
-    //        }
-    //    }
-    //}
-
-    //public Bullet GetUsableBullet()
-    //{
-    //    for (int i = 0; i < bulletPool.Count; i++)
-    //    {
-    //        if (bulletPool[i].gameObject.activeSelf == true) continue;
-    //        return bulletPool[i];
-    //    }
-
-    //    Debug.Log("BulletEmpty");
-    //    return null;
-    //}
-
-    // Use this for initialization
+   
 
 }
