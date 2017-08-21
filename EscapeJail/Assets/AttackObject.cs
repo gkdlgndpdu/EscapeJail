c8495bea5a73c612c45a473f760f2fe777ad3f23
DESKTOP-9313LHE\wer37 <wer3799@naver.com>
2017-08-21 10:17:49 +0900

    근접

---
+++ EscapeJail/Assets/AttackObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackObject : MonoBehaviour
+{
+    private int power = 1;
+
+    private int Initialize(int power)
+    {
+        this.power = power;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CharacterBase characterBase = collision.gameObject.GetComponent<CharacterBase>();
+            if (characterBase != null)
+                characterBase.GetDamage(this.power);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EscapeJail/Assets/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Tile : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
