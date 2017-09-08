c2db9b0e6d26f19164523db2f488fcbace1b9033
용진 <wer3799@naver.com>
2017-09-09 02:45:23 +0900

    q

---
+++ EscapeJail/Assets/StageInfoObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageInfoObject : ScriptableObject
+{
+    public int RoomNum;
+
+}
+
+
+public class StageData : ScriptableObject
+{
+    public List<GameObject> SpawnMonsterList;
+    public int roomNum;
+
+
+    public static StageData CreateInstance()
+    {
+        var data = ScriptableObject.CreateInstance<StageData>();      
+        return data;
+    }
+}
