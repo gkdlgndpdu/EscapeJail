cff9fa2a587e2819a91b90dcaf7fc680fee8d517
용진 <wer3799@naver.com>
2017-11-23 14:24:43 +0900

    패시브 기획 수정

--- EscapeJail/Assets/02.Scripts/DB/DatabaseLoader.cs
+++ EscapeJail/Assets/02.Scripts/DB/DatabaseLoader.cs
@@ -343,5 +343,84 @@ public class DatabaseLoader : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// //////////////////// //파일 저장 //파일 저장 //파일 저장 //파일 저장
+    /// 
+    /// </summary>
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveBtn();
+        }
+      
+    }
+
+    //파일 저장
+    public void SaveBtn()
+    {
+        StartCoroutine(SaveDb("ItemDb.sqlite"));
+
+    }
+    // 코루틴 .
+    IEnumerator SaveDb(string p)
+    {
+
+        string Filepath;
+#if UNITY_EDITOR
+        Filepath = Application.streamingAssetsPath + "/" + p;
+#else
+       Filepath = Application.persistentDataPath + "/" + p;
+#endif
+
+        if (!File.Exists(Filepath))
+        {
+            Debug.LogWarning("File \"" + Filepath + "\" does not exist. Attempting to create from \"" +
+                             Application.dataPath + "!/assets/" + p);
+
+            WWW loadDB = new WWW("jar:file://" + Application.dataPath + "!/assets/" + p);
+            while (!loadDB.isDone) { }
+            File.WriteAllBytes(Filepath, loadDB.bytes);
+        }
+        string connectionString = "URI=file:" + Filepath;
+      //  ItemList.Clear();
+
+        // using을 사용함으로써 비정상적인 예외가 발생할 경우에도 반드시 파일을 닫히도록 할 수 있다.
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())  // EnterSqL에 명령 할 수 있다. 
+            {
+
+                //수정
+                string ID = "2";
+                string Name = "박";
+                string Discription = "용진";
+
+                string sqlQuery = "UPDATE  ItemTable  SET " +
+                    "Name =" + "'" + Name + "'"
+                    + ",Desc =" + "'" + Discription + "'"+" "+"WHERE ID = "+ ID;
+
+                // string sqlQuery = "INSERT INTO ItemTable  (Name,Desc) VALUES('죽창','죽창이다.')";
+
+
+                //string sqlQuery = "DELETE FROM ItemTable Where ID =5";
+                // WHere을 붙인 이유는 테이블 전체를 돌기 때문에 해당 아이디만 수정하게 선택한것.
+                Debug.Log(sqlQuery);
+                //UPDATE UserInfo  SET  Money = 11, Scene ='dd', Pos ='0,0,1', Car ='0,0,1'
+
+                dbCmd.CommandText = sqlQuery;
+                using (IDataReader reader = dbCmd.ExecuteReader()) // 테이블에 있는 데이터들이 들어간다. 
+                {
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+        yield return null;
+    }
+
 
 }
--- EscapeJail/Assets/02.Scripts/Manager/LobbyManager.cs
+++ EscapeJail/Assets/02.Scripts/Manager/LobbyManager.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class LobbyManager : MonoBehaviour
 {
 
-
-
     public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync((int)SceneName.SelectScene);
-
-
-
     }
 
 }
