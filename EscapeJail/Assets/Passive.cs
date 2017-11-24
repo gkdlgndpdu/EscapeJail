2963698eda45148b1272c43764ef4bacc2f7a3e6
용진 <wer3799@naver.com>
2017-11-25 03:58:29 +0900

    머지용

---
+++ EscapeJail/Assets/Passive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Passive
+{
+    None,
+    AutoAim,
+    ExtendedMag,
+    ExtendedPocket,
+    ForeGrip,
+    Ginseng,
+    HolyCape,
+    Littlelove,
+    PigStatue,
+    RedDotSight,
+    RunAway,
+    Scouter,
+    VampiricGun,
+    WingShoes
+}
+
