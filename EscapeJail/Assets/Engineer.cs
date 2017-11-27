bb38d0f30f1ce922f631e54955264c58a56fb5ac
용진 <wer3799@naver.com>
2017-11-27 18:09:27 +0900

    엔지니어 (애니메이션 x)

--- EscapeJail/Assets/Engineer.cs
+++ EscapeJail/Assets/Engineer.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class Engineer : CharacterBase
 {
+    [SerializeField]
+    private Engineer_Drone drone;
+
+    private float skillCoolTimeMax = 10f;
+    private bool isSkillOn = true;
+
+    private IEnumerator skillCoolTimeRoutine()
+    {
+        float count = 0f;
+        while (true)
+        {
+            count += Time.deltaTime;
+
+            if (playerUi != null)
+                playerUi.SetSkillButtonProgress(count, skillCoolTimeMax);
+
+            if (count >= skillCoolTimeMax)
+            {
+                isSkillOn = true;
+                yield break;
+            }
+            yield return null;
+        }
+    }
 
     private new void Awake()
     {
         base.Awake();
-        SetHp(10);  
+        SetHp(10);
     }
     private new void Start()
     {
@@ -18,6 +42,24 @@ public class Engineer : CharacterBase
     private new void Update()
     {
         base.Update();
-   
+
+    }
+
+    public override void UseCharacterSkill()
+    {
+        if (isSkillOn == true)
+        {
+            UseSkill();
+        }
+    }
+
+    private void UseSkill()
+    {
+        isSkillOn = false;
+        StartCoroutine(skillCoolTimeRoutine());
+
+        //효과
+        //총알삭제
+        ObjectManager.Instance.AllEnemyBulletDestroy();
     }
 }
