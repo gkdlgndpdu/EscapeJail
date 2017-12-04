f8963dabfe61501359bcc2c94ed67e0e3baf15da
용진 <wer3799@naver.com>
2017-12-04 22:11:44 +0900

    수정사항

--- EscapeJail/Assets/FadeTextTest.cs
+++ EscapeJail/Assets/FadeTextTest.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeTextTest : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start () {
-        iTween.FadeTo(this.gameObject, iTween.Hash( "loopType", "pingPong", "alpha",0f));
-
-
+    private Text text;
+    private float fadeTime = 0.5f;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Start()
+    {
+        StartCoroutine(FadeRoutine());
+    }
+
+    private enum State
+    {
+        Down,Up
+    }
+    IEnumerator FadeRoutine()
+    {
+        if (text == null) yield break ;
+        float alpha = 1f;
+        State state = State.Down;
+        while (true)
+        {
+            if(state == State.Down)
+            {
+                text.color = new Color(1f, 1f, 1f, alpha);
+                alpha -= Time.deltaTime;
+                if (alpha <= 0)
+                {
+                    state = State.Up;
+                }
+            }
+            else
+            {
+                text.color = new Color(1f, 1f, 1f, alpha);
+                alpha += Time.deltaTime;
+                if (alpha>= 1)
+                {
+                    state = State.Down;
+                }
+            }
+      
+            yield return null;
+        }
+    }
+ 
+   
 }
