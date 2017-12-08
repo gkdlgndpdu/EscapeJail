a4cfb5bedc56a3c0bad09ee10b29258eda2473d5
용진 <wer3799@naver.com>
2017-12-08 19:07:50 +0900

    구글 로그인

---
+++ EscapeJail/Assets/GoogleService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+
+public class GoogleService : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayGamesPlatform.Instance.Authenticate((bool success) =>
+            {
+                if (success == true)
+                {
+                    //로그인 성공처리
+                }
+                else
+                {
+                    //로그인 실패처리
+                }
+            });
+   
+
+
+
+
+
+    }
+
+    public void SignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+    }
+
+
+}
