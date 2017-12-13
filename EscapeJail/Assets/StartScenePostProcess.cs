a0051ea30bff86e21613f4c8c65aa16fc504496a
용진 <wer3799@naver.com>
2017-12-14 03:55:57 +0900

    시작씬 효과

---
+++ EscapeJail/Assets/StartScenePostProcess.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class StartScenePostProcess : MonoBehaviour
+{
+
+    private PostProcessingProfile postProcessBehavior;
+
+    private BloomModel.Settings bloom;
+
+    private void Awake()
+    {
+        postProcessBehavior = GetComponent<PostProcessingBehaviour>().profile;
+
+        bloom = postProcessBehavior.bloom.settings;
+
+        bloom.bloom.intensity = 10f;
+    }
+    private void Start()
+    {
+        StartCoroutine(EffectRoutine());
+    }
+    private enum State
+    {
+        Down, Up
+    }
+
+    IEnumerator EffectRoutine()
+    {
+  
+        float alpha = 1f;
+        State state = State.Down;
+        while (true)
+        {
+            if (state == State.Down)
+            {
+                SetBrightnsee(alpha);
+                alpha -= Time.deltaTime;
+                if (alpha <= 0)
+                {
+                    state = State.Up;
+                }
+            }
+            else
+            {
+                SetBrightnsee(alpha);
+                alpha += Time.deltaTime;
+                if (alpha >= 1)
+                {
+                    state = State.Down;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private void SetBrightnsee(float value)
+    {
+        if (postProcessBehavior == null) return;
+        bloom = postProcessBehavior.bloom.settings;
+        bloom.bloom.intensity = value*5f;
+        postProcessBehavior.bloom.settings = bloom;
+    }
+
+
+}
