cda42dafa06ed90b04d1a17eb0db18f619831e96
용진 <wer3799@naver.com>
2017-12-18 23:31:47 +0900

    캐릭터 언락

--- EscapeJail/Assets/02.Scripts/DB/DatabaseLoader.cs
+++ EscapeJail/Assets/02.Scripts/DB/DatabaseLoader.cs
@@ -481,6 +481,7 @@ public class DatabaseLoader : MonoBehaviour
         ChangePassiveDB(passiveType);
 
     }
+
     // 코루틴 .
     private void ChangePassiveDB(PassiveType passiveType)
     {
@@ -542,6 +543,100 @@ public class DatabaseLoader : MonoBehaviour
 
 
     }
+    public void BuyCharacter(CharacterType characterType)
+    {
+        ChangeCharacterDB(characterType);
+    }
+
+    private void ChangeCharacterDB(CharacterType characterType)
+    {
+
+        string Filepath;
+#if UNITY_EDITOR
+        Filepath = Application.streamingAssetsPath + "/" + "CharacterDB.db";
+#else
+       Filepath = Application.persistentDataPath + "/" + "CharacterDB.db";
+#endif
+
+        if (!File.Exists(Filepath))
+        {
+            Debug.LogWarning("File \"" + Filepath + "\" does not exist. Attempting to create from \"" +
+                             Application.dataPath + "!/assets/" + "CharacterDB.db");
+
+            WWW loadDB = new WWW("jar:file://" + Application.dataPath + "!/assets/" + "CharacterDB.db");
+            while (!loadDB.isDone) { }
+            File.WriteAllBytes(Filepath, loadDB.bytes);
+        }
+        string connectionString = "URI=file:" + Filepath;
+        //  ItemList.Clear();
+
+        // using을 사용함으로써 비정상적인 예외가 발생할 경우에도 반드시 파일을 닫히도록 할 수 있다.
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())  // EnterSqL에 명령 할 수 있다. 
+            {
+
+                //수정
+                string ID = ((int)characterType).ToString();
+                string Boolean = "True";
+
+                string sqlQuery = "UPDATE  CharacterDB  SET " +
+                    "HasCharacter =" + "'" + Boolean + "'"
+                    + "WHERE ID = " + ID;
+
+                // string sqlQuery = "INSERT INTO ItemTable  (Name,Desc) VALUES('죽창','죽창이다.')";
+
+
+                //string sqlQuery = "DELETE FROM ItemTable Where ID =5";
+                // WHere을 붙인 이유는 테이블 전체를 돌기 때문에 해당 아이디만 수정하게 선택한것.
+                Debug.Log(sqlQuery);
+                //UPDATE UserInfo  SET  Money = 11, Scene ='dd', Pos ='0,0,1', Car ='0,0,1'
+
+                dbCmd.CommandText = sqlQuery;
+                using (IDataReader reader = dbCmd.ExecuteReader()) // 테이블에 있는 데이터들이 들어간다. 
+                {
+                    dbConnection.Close();
+                    reader.Close();
+                }
+            }
+        }
+
+        //DB재갱신
+        LoadCharacterDB();
+
+
+    }
+
+#if UNITY_EDITOR
+    //임시코드
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            BuyCharacter(CharacterType.Scientist);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            BuyCharacter(CharacterType.Defender);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            BuyCharacter(CharacterType.Sniper);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            BuyCharacter(CharacterType.Engineer);
+
+        }
+    }
+
+#endif
+
 
 
 }
