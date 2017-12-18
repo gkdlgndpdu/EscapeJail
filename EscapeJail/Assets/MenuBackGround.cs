9c418368d56fb4934bb0ca65ffc3c343a8a0485f
용진 <wer3799@naver.com>
2017-12-18 22:26:44 +0900

    메뉴ㅜ화면

---
+++ EscapeJail/Assets/MenuBackGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuBackGround : MonoBehaviour
+{
+    [SerializeField]
+    private Image redImage;
+
+
+    IEnumerator RedImageFade()
+    {
+        if (redImage == null) yield break;
+        while (true)
+        {
+            redImage.gameObject.SetActive(!redImage.gameObject.activeSelf);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(RedImageFade());
+	}
+	
+	
+}
